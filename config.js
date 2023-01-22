125004f7e1e25cb21a68ef432b53a3f6f718591d
ZinniaMaika <116232920+ZinniaMaika@users.noreply.github.com>
2023-01-22 07:39:41 -0600

    Update config.js

--- config.js
+++ config.js
@@ -8,22 +8,22 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['573225236629', '💫 Mystic - Collaborator 4 💫', true],
-  ['593959425714', '💫 Mystic - Collaborator 5 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 6 💫', true],  
-  ['5212411719888', '💫 Mystic - Collaborator 7 💫', true],	
-  ['593968585283'],
-  ['5219993404349'],
-  ['5219991402134'],	
-  ['5219996125657']
+  ['50589847764', '👑 Mystic - Creador 👑', true],
+  ['50582992786', '💫 Mystic - Collaborator 1 💫', true],
+  ['447361591502', '💫 Mystic - Collaborator 2 💫', true],
+  ['', '💫 Mystic - Collaborator 3 💫', true],
+  ['', '💫 Mystic - Collaborator 4 💫', true],
+  ['', '💫 Mystic - Collaborator 5 💫', true],
+  ['', '💫 Mystic - Collaborator 6 💫', true],  
+  ['', '💫 Mystic - Collaborator 7 💫', true],	
+  [''],
+  [''],
+  [''],	
+  ['']
 ] 
 
-global.suittag = ['5219993404349'] 
-global.prems = ['51995386439'] 
+global.suittag = ['50589847764'] 
+global.prems = ['50589847764'] 
 
 global.packname = '(☞ﾟ∀ﾟ)☞'
 global.author = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
